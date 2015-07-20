cda955fdd653e55b6accc23b385668f85a14cad6
junjielee <mail.lijunjie@qq.com>
2015-07-21 00:23:13 +0800

    处理链接mongoengine问题，现在后台总算可以启动了；在本地运行也可以了；

--- admin.py
+++ admin.py
@@ -1,26 +1,16 @@
 # coding: utf8
 
-# from flask import Flask
 # from flask_admin import Admin
-
-# app = Flask(__name__)
-
-# admin = Admin(app)
-
-# if __name__ == '__main__':
-    # app.run()
-
+# from flask_admin.contrib.mongoengine import ModelView
 from flask import Flask
-from flask_admin import Admin
-from flask.ext import admin
+from flask.ext.admin import Admin, AdminIndexView
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.admin.contrib.mongoengine import ModelView
+from flask.ext.admin import expose
 import config
 import models
 
 app = Flask(__name__)
-
-ad = Admin(app)
 
 app.config.from_object(config.Config)
 
@@ -28,35 +18,40 @@ db = MongoEngine()
 db.init_app(app)
 
 
-# view
 class UserView(ModelView):
     column_searchable_list = ('name', 'mobile')
     column_default_sort = ('time', True)
 
 
 class MerchantView(ModelView):
-    column_default_sort = ('time', True)
+    pass
 
 
 class CategoryView(ModelView):
-    column_default_sort = ('time', True)
+    pass
 
 
 class ProductView(ModelView):
-    column_default_sort = ('time', True)
+    pass
 
 
 class OrderView(ModelView):
-    column_default_sort = ('time', True)
+    pass
 
+
+class IndexView(AdminIndexView):
+    @expose('/', methods=['GET', 'POST'])
+    def index(self):
+        return self.render('index.html')
+
+
+panel = Admin(app, name='Golden', index_view=IndexView(name=u'主页'))
+panel.add_view(UserView(models.User, name=u'用户'))
+panel.add_view(MerchantView(models.Merchant, name=u'商家'))
+panel.add_view(CategoryView(models.Category, name=u'类别'))
+panel.add_view(ProductView(models.Product, name=u'产品'))
+panel.add_view(OrderView(models.Order, name=u'订单'))
 
 if __name__ == '__main__':
-    panel = admin.Admin(app)
-    panel.add_view(UserView(models.User))
-    panel.add_view(MerchantView(models.Merchant))
-    panel.add_view(CategoryView(models.Category))
-    panel.add_view(ProductView(models.Product))
-    panel.add_view(OrderView(models.Order))
-
     # app.run(host='127.0.0.1', port=2050)
     app.run()
--- config.py
+++ config.py
@@ -2,7 +2,11 @@
 
 
 class Config(object):
-    SECRET_KEY = 'koala@2015'
+    SECRET_KEY = 'golden@2015'
     DEBUG = True
 
-    MONGODB_SETTINGS = {'DB': 'koala'}
+    MONGODB_SETTINGS = {
+        'db': 'golden',
+        'host': '127.0.0.1',
+        'port': 27017
+    }
