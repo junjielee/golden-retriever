204d0b9ae149b9957301153ab3ba05ada14e6635
junjielee <mail.lijunjie@qq.com>
2015-07-26 22:53:55 +0800

    终于可以用模版了；被frozenui的最新版坑了。

--- app.py
+++ app.py
@@ -1,15 +1,50 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask
+from flask import Flask, render_template, url_for
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 
-@application.route('/')
+@app.route('/')
 def index():
-    return 'Hello, my name is lijunjie.'
+    return render_template('app/index.html')
+
+
+@app.route('/login')
+def login():
+    return 'Login Page'
+
+
+@app.route('/logout')
+def logout():
+    return 'Logout Page'
+
+
+@app.route('/signup')
+def signup():
+    return 'Index Page'
+
+
+@app.route('/order')
+def order():
+    return render_template('app/order.html')
+
+
+@app.route('/order/detail')
+def order_detail():
+    return 'Order detail page'
+
+
+@app.route('/product')
+def product():
+    return render_template('app/product.html')
+
+
+@app.route('/product/detail')
+def product_detail():
+    return 'Product detail page'
 
 if __name__ == '__main__':
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run(host='127.0.0.1', port=8000)
